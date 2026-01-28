a369ba261df86b8b651b9fb8cd26061ab9c42abd
Ivan Yadchyshyn <yad7devadress@gmail.com>
2026-01-28 10:26:49 +0200

    Create BlazorDeepDive project to learn static SSR in blazor

---
+++ BlazorDeepDive/ServerManagement/Models/Server.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServerManagement.Models
+{
+    public class Server
+    {
+        public Server()
+        {
+            Random rand = new Random();
+            int randomNumber = rand.Next(0, 2);
+            IsOnline = randomNumber == 0;
+        }
+
+        public int Id { get; set; }
+        public bool IsOnline { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        
+        [Required]
+        public string City { get; set; }
+    }
+}
